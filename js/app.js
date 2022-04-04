b4c6146ef5f55fa21f56bc77dacc6a55a05e48c9
Maccabee Levine <msl321@lehigh.edu>
2022-04-04 11:31:00 -0400

    Add login and search for local holdings

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
   initUser();
-  initListener();
+  initListeners();
+
+  bootstrap();
 });
+
+function bootstrap() {
+  let params = new URLSearchParams(window.location.search);
+  if (params.has("username")) {
+    let username = params.get('username');
+    $("#username_input").val(username);
+    $("#login_button").click();
+  }
+}
 
 function initUser() {
   $.ajaxSetup({
@@ -11,10 +22,111 @@ function initUser() {
   });
 }
 
-function initListener() {
+function initListeners() {
+  $("#login_input_section").on("submit", function(event) {
+    event.preventDefault();
+    $("login_button").click();
+  });
+  $("#login_button").on("click", function(event) {
+    login();
+  });
+
+  $("#item_input_section").on("submit", function(event) {
+    event.preventDefault();
+    $("search_button").click();
+  });
+  $("#search_button").on("click", function(event) {
+    search();
+  });
+
+  $("#reject_local_holdings_button").on("click", function(event) {
+    rejectLocalHoldings();
+  });
+
   $("#submit_button").on("click", function(event) {
     sendNewRequest();
   });
+}
+
+function login() {
+  // hide login input
+  $("#login_input_section").hide();
+
+  // show login done
+  $("#username").text($('#username_input').val());
+  $("#login_done_section").show();
+
+  // show item input
+  $("#item_input_section").show();
+
+  bootstrapSearch();
+}
+
+function bootstrapSearch() {
+  let params = new URLSearchParams(window.location.search);
+  if (params.has("title")) {
+    $("#title_input").val(params.get('title'));
+  }
+  if (params.has("contributor")) {
+    $("#contributor_input").val(params.get('contributor'));
+  }
+}
+
+function search() {
+  let title = normalize($('#title_input').val());
+  let contributor = normalize($('#contributor_input').val());
+  let format = $("#book_format input:checked").val();
+  let request_data = {
+    title: title,
+    contributor: contributor,
+    format: format,
+  };
+
+  $.ajax({
+    url: "http://localhost:8080/search-matches?" + new URLSearchParams(request_data),
+    method: "GET",
+    contentType: 'application/json',
+    success: function(data, status, xhr) {
+      console.log("search call succeeded");
+      searchSucceeded(data);
+    },
+    error: function(xhr, status, error) {
+      console.log("search failed with status " + status + " and error: " + error);
+    }
+  });
+}
+
+function searchSucceeded(data) {
+  // hide item input
+  $("#item_input_section").hide();
+
+  // show the item searched for
+  $("#title").text($("#title_input").val());
+  $("#contributor").text($("#contributor_input").val());
+  $("#search_done_section").show();
+
+  console.log("number of results: " + data.length);
+  if (data.length > 0) {
+    // show the search results
+    $("#local_results").empty();
+    for (let i=0; i < data.length; i++) {
+      let result = data[i];
+      let url = CATALOG_URL_FOR_OCLC_NUMBER.replace("{oclc}", result.oclcNumber);
+      let link = $("<a>").attr("href", url).attr("target", "_blank").text(result.title);
+      let local_item = $("<li>").append(link).append(" by ").append(result.contributor);
+      $("#local_results").append(local_item);
+    }
+    $("#local_holdings_input_section").show();
+  }
+  else {
+    // show delivery input
+    $("#delivery_input_section").show();
+  }
+}
+
+function rejectLocalHoldings() {
+  $("#local_holdings_input_section").hide();
+  $("#delivery_input_section").show();
 }
 
 function sendNewRequest() {
